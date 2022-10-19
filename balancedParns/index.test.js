693348af8f051c3184e771a58ba7a7a0f1312e2f
emad-almajdalawi <e.almajdalawi@gamil.com>
2022-10-19 11:24:02 +0300

    Feat: create stack and the function

--- balancedParns/index.test.js
+++ balancedParns/index.test.js
@@ -24,6 +24,69 @@
  *	"())"
  */
 
-function balancedParens(input) {
-  //  TO DO
+
+class Node {
+  constructor(value) {
+    this.value = value
+    this.next = null
+  }
 }
+
+
+class Stack {
+  constructor() {
+    this.top = null
+  }
+
+  isEmpty() {
+    if (this.top === null) return true
+    else return false
+  }
+
+  push(newValue) {
+    if (newValue instanceof Node) return 'Please enter a value and it will converted to Node automaticly'
+
+    let newNode = new Node(newValue)
+    newNode.next = this.top
+    this.top = newNode
+  }
+
+  pop() {
+    if (this.isEmpty()) return 'The stack is empty!'
+
+    let oldTop = this.top
+    this.top = this.top.next
+    oldTop.next = null
+
+    return oldTop.value
+  }
+}
+
+
+function balancedParens(input) {
+  let inputArr = input.split('')
+  if (inputArr.length < 1) return 'Empty string!'
+
+  let stack = new Stack()
+
+  inputArr.forEach(char => {
+    if (char == '(' || char == '{' || char == '[') stack.push(char)
+    else if (char == ')' || char == '}' || char == ']') {
+      if (stack.isEmpty()) return false
+      else {
+        if (char == ')' && stack.top.value == '(') stack.pop()
+        else if (char == '}' && stack.top.value == '{') stack.pop()
+        else if (char == ']' && stack.top.value == '[') stack.pop()
+        else return false
+      }
+    }
+  })
+
+  if (stack.isEmpty()) return true
+  else return false
+}
+
+
+console.log(balancedParens('[()]'))
+console.log(balancedParens('[(])'))
+console.log(balancedParens(''))
