5d71fa6264c69c507d0a52984e3944a9991932d4
emad-almajdalawi <e.almajdalawi@gamil.com>
2022-10-31 23:00:28 +0300

    Feat: solusion

--- composePipe/index.test.js
+++ composePipe/index.test.js
@@ -32,3 +32,68 @@
   var addAndMultiplyTwice = pipe(add2, multiplyBy3, multiplyBy3);
   addAndMultiplyTwice(5); //should be 63
  */
+
+
+var greet = function (name) { return 'hello ' + name; }
+var exclaim = function (statement) { return statement.toUpperCase() + '!'; }
+var abc = function (x) { return 'hi ' + x }
+var xyz = function (x) { return 'second ' + x }
+
+
+
+function compose(...args) {
+  return x
+
+  function x(str) {
+    let func1
+    let func2
+    for (let i = args.length - 1; i >= 0; i -= 2) {
+      if (i == args.length - 1) {
+        func1 = args[i](str)
+        if (i - 1 >= 0) {
+          func2 = args[i - 1](func1)
+        } else {
+          return func1
+        }
+      } else {
+        func1 = args[i](func2)
+        if (i - 1 >= 0) {
+          func2 = args[i - 1](func1)
+        } else {
+          return func1
+        }
+      }
+    }
+    return func2
+  }
+}
+
+function pipe(...args) {
+  return x
+
+  function x(str) {
+    let func1
+    let func2
+    for (let i = 0; i < args.length; i += 2) {
+      if (i == 0) {
+        func1 = args[i](str)
+        if (i + 1 <= args.length - 1) {
+          func2 = args[i + 1](func1)
+        } else {
+          return func1
+        }
+      } else {
+        func1 = args[i](func2)
+        if (i + 1 <= args.length - 1) {
+          func2 = args[i + 1](func1)
+        } else {
+          return func1
+        }
+      }
+    }
+    return func2
+  }
+}
+
+console.log(compose(greet, exclaim, abc, xyz)('emad'))
+console.log(pipe(greet, exclaim, abc, xyz)('emad'))
