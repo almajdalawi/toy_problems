6876024b840ec070acd24b0c88193a7dca3642ae
emad-almajdalawi <e.almajdalawi@gamil.com>
2022-11-06 14:55:16 +0300

    Feat: solution with tests

--- factorial/index.test.js
+++ factorial/index.test.js
@@ -7,13 +7,14 @@
  */
 
 function factorial(number) {
-  return (number < 2 ) ? 1 : number * factorial(number - 1);
-  // TO DO
+  if (number < 0) { return 'Negative numbers are not allowed!' }
+  return (number < 2) ? 1 : number * factorial(number - 1);
 }
 
 
 describe('Tests', () => {
   it('test factorial', () => {
+    expect(factorial(-1)).toEqual('Negative numbers are not allowed!');
     expect(factorial(0)).toEqual(1);
     expect(factorial(1)).toEqual(1);
     expect(factorial(5)).toEqual(120);
